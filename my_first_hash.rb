a5ead7ecffc824103c3bef09f863051ea879e499
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-28 18:56:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,11 +18,11 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale_bone_corsets" => 5, 
-                  "porcelain_vases" => 2, 
-                  "oil_paintings" => 3
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
                  }
   shipping_manifest["muskets"] = 2
-  shipping_manifest["gun_powder"] = 4
+  shipping_manifest["gun powder"] = 4
   shipping_manifest
 end
