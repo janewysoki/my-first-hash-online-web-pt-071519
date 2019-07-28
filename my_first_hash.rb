6be3b274ba33508c5f26e05db3fcf67e472fbe6a
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-28 18:54:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,23 +4,23 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"whale_bone_corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  the_manifest = {"whale_bone_corsets" => 5, "porcelain_vases" => 2, "oil_paintings" => 3}
 end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "whale_bone_corsets" => 5, 
+                  "porcelain_vases" => 2, 
+                  "oil_paintings" => 3
                  }
-  shipping_manifest["oil paintings"]
+  shipping_manifest["oil_paintings"]
 end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "whale_bone_corsets" => 5, 
+                  "porcelain_vases" => 2, 
+                  "oil_paintings" => 3
                  }
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun_powder"] = 4
