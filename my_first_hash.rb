643204bc194b3134d7e5bfbd480a666dc0f36691
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-28 18:55:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,7 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"whale_bone_corsets" => 5, "porcelain_vases" => 2, "oil_paintings" => 3}
+  shipping_manifest = {"whale_bone_corsets" => 5, "porcelain_vases" => 2, "oil_paintings" => 3}
 end
 
 def retrieval
